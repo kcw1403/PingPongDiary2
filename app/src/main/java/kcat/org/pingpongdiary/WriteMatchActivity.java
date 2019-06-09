266ff067fd04dffb70fca9975378e452ad944dd5
kim <kcw1403@gmail.com>
2019-06-09 16:25:15 +0900

    test

--- app/src/main/java/kcat/org/pingpongdiary/WriteMatchActivity.java
+++ app/src/main/java/kcat/org/pingpongdiary/WriteMatchActivity.java
@@ -9,5 +9,5 @@ public class WriteMatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_match);
-    }
+}
 }
